8fccb20a6204f5e076fcd242797d6c0add159c7f
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-10 19:48:09 +0500

    Task1

--- Tyuiu.GilevaAp.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.GilevaAp.Sprint0.Task0.V0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        string name = "Igor";
+        int age = 45;
+        Console.WriteLine("здравствуйте меня зовут " + name + " мне " + age + " лет");
+        Console.ReadKey();
+    }
+}
