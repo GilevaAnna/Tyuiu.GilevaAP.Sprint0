cffde11c97d4af4e12dc82f201a1bf2639349660
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-12 11:58:23 +0500

    Task6

---
+++ Tyuiu.GilevaAP.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GilevaAP.Sprint0.Task6.V0.lib;
+namespace Tyuiu.GilevaAP.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = "+DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разеость элементов массива = "+DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массиво = "+DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
