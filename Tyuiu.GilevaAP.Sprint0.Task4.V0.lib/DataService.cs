16a10c042d79641c7afb045481d7287f58ebedd9
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-12 09:31:34 +0500

    Task4

---
+++ Tyuiu.GilevaAP.Sprint0.Task4.V0.lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿namespace Tyuiu.GilevaAP.Sprint0.Task4.V0.lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {  
+            return a + b; 
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b) 
+        {
+            return (a / b);
+        }
+    }
+}
